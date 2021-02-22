02e4d0c4411496bdaa8ddcb610e52bfb5c0686f0
Josh Aune <josh.aune@hammerspace.com>
2021-02-22 15:39:27 -0700

    very basic cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+#!/usr/bin/venv python
+from click.testing import CliRunner
+import hstk.hscli as hscli
+
+def test_cli_loads():
+    runner = CliRunner()
+    res = runner.invoke(hscli.cli)
+    assert res.exit_code == 0
+
+def test_simple_eval():
+    runner = CliRunner()
+    res = runner.invoke(hscli.cli, '-nv eval -e THIS .'.split())
+    assert res.exit_code == 0
+
